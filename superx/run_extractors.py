72578b94df63b67c67c2b2c09044a06667dff4b3
aryehlev <59808808+aryehlev@users.noreply.github.com>
2020-11-08 00:34:39 +0200

    try

--- superx/run_extractors.py
+++ superx/run_extractors.py
@@ -3,5 +3,5 @@ from information_extractors import InfoExtractor, BranchExtractor
 i_e = InfoExtractor()
 b_e = BranchExtractor()
 
-i_e.run_info_extractor
-b_e.run_branch_extractor
+i_e.run_info_extractor()
+b_e.run_branch_extractor()
